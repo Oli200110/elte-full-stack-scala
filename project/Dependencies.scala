cda0376416c077eeae860d79fef1c4f8ca857aed
zoltan.zvara <zoltan.zvara@enliven.systems>
2022-10-07 11:28:28 +0200

    Add AKKA dependencies & add basic HTTP routing.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,28 @@ object Dependencies {
     "org.scala-lang.modules" %% "scala-collection-contrib" % "0.2.2",
     "org.scala-lang.modules" %% "scala-parallel-collections" % "1.0.4",
     "org.scalatest" %% "scalatest" % "3.2.12" % "test",
-    "com.typesafe.scala-logging" %% "scala-logging" % "3.9.5"
+    "com.typesafe.scala-logging" %% "scala-logging" % "3.9.5",
+    "com.typesafe.akka" %% "akka-actor" % "2.6.19",
+    "com.typesafe.akka" %% "akka-slf4j" % "2.6.19",
+    "com.typesafe.akka" %% "akka-stream" % "2.6.19",
+    "com.typesafe.akka" %% "akka-cluster" % "2.6.19",
+    "com.typesafe.akka" %% "akka-cluster-tools" % "2.6.19",
+    "com.typesafe.akka" %% "akka-cluster-metrics" % "2.6.19",
+    "com.typesafe.akka" %% "akka-stream-typed" % "2.6.19",
+    "com.typesafe.akka" %% "akka-actor-typed" % "2.6.19",
+    "com.typesafe.akka" %% "akka-cluster-typed" % "2.6.19",
+    "com.typesafe.akka" %% "akka-cluster-sharding-typed" % "2.6.19",
+    "com.typesafe.akka" %% "akka-stream-typed" % "2.6.19",
+    "com.typesafe.akka" %% "akka-discovery" % "2.6.19",
+    "com.lightbend.akka.management" %% "akka-management-cluster-bootstrap" % "1.1.3",
+    "com.lightbend.akka.discovery" %% "akka-discovery-kubernetes-api" % "1.1.3",
+    "com.typesafe.akka" %% "akka-stream-testkit" % "2.6.19",
+    "com.typesafe.akka" %% "akka-stream-testkit" % "2.6.19" % "test",
+    "com.typesafe.akka" %% "akka-http" % "10.2.9",
+    "com.typesafe.akka" %% "akka-http-core" % "10.2.9",
+    "com.typesafe.akka" %% "akka-http-testkit" % "10.2.9",
+    "com.typesafe.akka" %% "akka-http-spray-json" % "10.2.9",
+    "com.typesafe.akka" %% "akka-http-testkit" % "10.2.9" % "test"
   )
 
 }
